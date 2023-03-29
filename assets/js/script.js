ebf4db9571e62156c6ee5c05f523c5b915f22e2c
Samind2 <suphaneerangsirat@gmail.com>
2023-03-29 20:49:19 +0700

    29/3/66

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,91 @@
-const addEventOnElment = (elements, evenType, callback)=>{
-    console.log("add")
-    for (let i = 0, len = elements.length; i<len; i++) {
-        elements[i].addEventListener(evenType, callback);
+const addEventOnElements = (elements, evenType, callback) => {
+    for (let i = 0, len = elements.length; i < len; i++) {
+      elements[i].addEventListener(evenType, callback);
     }
-}
-
-
-
-
-
-/**
- * NavBar toggle
- */
-const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-const overlay = document.querySelector("[data-overlay]")
-console.log(navTogglers)
-
-const toggleNavBar = ()=>{
-    console.log("click")
+  };
+  
+  /**
+   * NavBar toggle
+   */
+  const navbar = document.querySelector("[data-navbar]");
+  const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+  const overlay = document.querySelector("[data-overlay]");
+  
+  const toggleNavbar = () => {
     navbar.classList.toggle("active");
     overlay.classList.toggle("active");
     document.body.classList.toggle("nav-active");
-}
-addEventOnElment(navTogglers, "click", toggleNavBar);
-
-/**
- * slider
- */
-const slider = document.querySelectorAll("[data-slider]");
-
-const initSlider = (currentSlider) => {
-    const sliderContainer = currentSlider.querySelector("[data-slider-container]");
+  };
+  addEventOnElements(navTogglers, "click", toggleNavbar);
+  
+  /**
+   * Slider
+   */
+  const sliders = document.querySelectorAll("[data-slider]");
+  
+  const initSlider = (currentSlider) => {
+    const sliderContainer = currentSlider.querySelector(
+      "[data-slider-container]"
+    );
     const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
     const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
-    let currentSliderpo = 0;
-
-    const moveSliderItem = () =>{
-        sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSliderpo].offsetLeft}px)`;
+    let currentSliderPo = 0;
+  
+    const moveSliderItem = () => {
+      sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSliderPo].offsetLeft}px)`;
     };
-
-    const sliderPrev = () =>{
-        if (currentSliderpo <= 0) {
-            currentSliderpo = sliderContainer.childElementCount -1;
-        }else{
-            currentSliderpo--;
-        }
-        moveSliderItem();
+    // Previous
+    const slidePrev = () => {
+      if (currentSliderPo <= 0) {
+        currentSliderPo = sliderContainer.childElementCount - 1;
+      } else {
+        currentSliderPo--;
+      }
+      moveSliderItem();
     };
-    sliderPrevBtn.addEventListener("click", sliderPrev);
-
+    sliderPrevBtn.addEventListener("click", slidePrev);
+  
     //Next
-    const sliderNext = () => {
-        const sliderEnd = currentSliderpo >= sliderContainer.childElementCount -1;
-
-        if (sliderEnd) {
-            currentSliderpo = 0;
-        }else{
-            currentSliderpo++;
-        }
-        moveSliderItem();
+    const slideNext = () => {
+      const slideEnd = currentSliderPo >= sliderContainer.childElementCount - 1;
+  
+      if (slideEnd) {
+        currentSliderPo = 0;
+      } else {
+        currentSliderPo++;
+      }
+      moveSliderItem();
     };
-    sliderNextBtn.addEventListener("click", sliderNext);
-
-
-    //No more slide
+    sliderNextBtn.addEventListener("click", slideNext);
+  
+    //No more Slide
     const dontHaveExtraItem = sliderContainer.childElementCount <= 1;
     if (dontHaveExtraItem) {
-        sliderNextBtn.style.display = "none";
-        sliderPrevBtn.style.display = "none";
+      sliderNextBtn.style.display = "none";
+      sliderPrevBtn.style.display = "none";
     }
-    } ;
+  };
+  
+  for (let i = 0, len = sliders.length; i < len; i++) {
+    initSlider(sliders[i]);
+  }
 
-    for(let i=0, len = slider.length; i<len; i++){
-        initSlider(slider[i]);
-    }
-   
-    //Accordian
 
-    const accordians = document.querySelectorAll("[data-accordian]");
-    let lasActiveAccordian = accordians[0];
-    const initAccordian = (currentAccordian) =>{
-        const accordianBtn = currentAccordian.querySelector("[data-accordian-btn]");
-        const expanAccordain = () =>{
-            if(lasActiveAccordian && lasActiveAccordian != currentAccordian) {
-                lasActiveAccordian.classList.remove("exapanded");
-            }
-            currentAccordian.classList.toggle("exapanded");
-            lasActiveAccordian = currentAccordian;
-        }
-        accordianBtn.addEventListener("click", expanAccordain);
+  //Accordian
+
+  const accordians = document.querySelectorAll("[data-accordian]");
+  let lastActiveAccordian = accordians[0];
+  const initAccordian = (currentAccordian) =>{
+    const accordianBtn = currentAccordian.querySelector("[data-accordian-btn]");
+    const expandAccordian = () =>{
+      if(lastActiveAccordian && lastActiveAccordian != currentAccordian) {
+        lastActiveAccordian.classList.remove("expanded");
+      }
+      currentAccordian.classList.toggle("expanded");
+      lastActiveAccordian = currentAccordian;
     };
-    for(let i=0, len=accordians.length; i<len;i++){
-        initAccordian(accordians[i]);
-    }
+    accordianBtn.addEventListener("click", expandAccordian);
+  };
+  for(let i =0, len = accordians.length; i<len;i++){
+    initAccordian(accordians[i]);
+  }
